d22adfa3323fa1f25acd87b121823f7a3b031a76
Erik Opinaldo <erikopinaldo@gmail.com>
2022-08-15 20:04:12 -0400

    colors now write to db

--- server.js
+++ server.js
@@ -3,16 +3,32 @@ const express = require('express');
 const { allowedNodeEnvironmentFlags } = require('process');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(8000, () => console.log('Listening on port 8000'))
 
+let mongoConnectionString = 'mongodb+srv://eopinaldo:yFZduDPXdv4bMjLgqm4X@cluster0.c6bao1l.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(mongoConnectionString)
+.then(client => {
+    console.log('Connected to DB')
+    const db = client.db('keyfigurator')
+    const colorsCollection = db.collection('colors')
+
+    app.post('/save-color', (req, res) => {
+        colorsCollection.insertOne(req.body)
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => console.error(error))
+    })
+})
+.catch(error => console.error(error))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html')
-})
-
-app.post('/save-color', (req, res) => {
-    console.log(req.body)
 })
