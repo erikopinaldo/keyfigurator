001b34cbd4c21e8e9cc70de124a32731a8233cda
Erik Opinaldo <erikopinaldo@gmail.com>
2022-08-23 01:10:30 -0400

    added mongoose schema

--- models/Colors.js
+++ models/Colors.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose')
 
-const ColorsSchema = new mongoose.Schema()
+const ColorsSchema = new mongoose.Schema({
+    backgroundColor: {
+        type: String,
+        required: true,
+    }, 
+    caseColor: {
+        type: String,
+        required: true,
+    },
+    keysColor: {
+        type: String,
+        required: true,
+    }, 
+    colorName: {
+        type: String,
+        required: true,
+    }
+})
 
-module.exports = mongoose.model('colors', ColorsSchema, 'colors')
+module.exports = mongoose.model('colors', ColorsSchema)
