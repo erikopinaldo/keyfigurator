de33ab486979b328d79754e9f6c565ef1e3cc0d2
Erik Opinaldo <erikopinaldo@gmail.com>
2022-08-22 20:17:09 -0400

    updated get request to use getColors method

--- routes/colors.js
+++ routes/colors.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const colorsController = require('../controllers/colors')
 
-router.get('/', colorsController.getTodos)
+router.get('/', colorsController.getColors)
 
 router.post('/createColor', colorsController.createColor)
 
