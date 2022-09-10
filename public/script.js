9ce282db516513e87c9ceade4e27bb8d3ac3d9f5
Erik Opinaldo <erikopinaldo@gmail.com>
2022-09-10 14:00:50 -0400

    deleteColor itemText variable fixed after HTML changes to button

--- public/script.js
+++ public/script.js
@@ -6,8 +6,9 @@ Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteColor)
 })
 
-async function deleteColor(){
-    const itemText = this.parentNode.childNodes[1].innerText
+async function deleteColor() {
+    console.log(this.parentNode.parentNode.childNodes)
+    const itemText = this.parentNode.parentNode.parentNode.childNodes[1].innerText
     console.log(itemText)
     try {
         const response = await fetch('deleteColor', {
