01c1908f9c1a1e02264c82abf0072a89bb1e3726
Erik Opinaldo <erikopinaldo@gmail.com>
2022-09-15 16:23:59 -0400

    removed color save and delete functions from colors route

--- routes/colors.js
+++ routes/colors.js
@@ -4,8 +4,4 @@ const colorsController = require('../controllers/colors')
 
 router.get('/', colorsController.getColors)
 
-router.post('/saveColor', colorsController.createColor)
-
-router.delete('/deleteColor', colorsController.deleteColor)
-
 module.exports = router
