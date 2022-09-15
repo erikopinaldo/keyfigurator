5d8c5cb8a7ec561a1e002d7373875a93090f16e7
Erik Opinaldo <erikopinaldo@gmail.com>
2022-09-15 16:58:56 -0400

    added login and signup routes

--- routes/colors.js
+++ routes/colors.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const authController = require('../controllers/auth')
 const colorsController = require('../controllers/colors')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', colorsController.getIndex)
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+router.get('/logout', authController.logout)
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
 
 module.exports = router
