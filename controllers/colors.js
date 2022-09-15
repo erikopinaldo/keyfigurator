ba8eb2a00943a9be04ba2c9702106cc7581709b6
Erik Opinaldo <erikopinaldo@gmail.com>
2022-09-15 16:28:06 -0400

    removed save and delete colors methods from colors controller

--- controllers/colors.js
+++ controllers/colors.js
@@ -9,31 +9,5 @@ module.exports = {
         }catch(err){
             console.log(err)
         }
-    },
-    createColor: async (req, res)=>{
-        try{
-            console.log(req.body)
-            await Color.create(
-                {
-                    backgroundColor: req.body.backgroundColor, 
-                    caseColor: req.body.caseColor, 
-                    keysColor: req.body.keysColor, 
-                    colorName: req.body.colorName
-                })
-            console.log('Color has been added!')
-            res.redirect('/')
-        }catch(err){
-            console.log(err)
-        }
-    },
-    deleteColor: async (req, res)=>{
-        console.log('req body ', req.body)
-        try{
-            await Color.findOneAndDelete({_id: req.body.itemFromJS})
-            console.log('Deleted Color')
-            res.json('Deleted It')
-        }catch(err){
-            console.log(err)
-        }
     }
 }    
