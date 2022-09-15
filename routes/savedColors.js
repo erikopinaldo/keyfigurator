c98b08e4b936c4e06d3d55000348f66be8abd53c
Erik Opinaldo <erikopinaldo@gmail.com>
2022-09-15 17:01:17 -0400

    added ensureAuth step before serving /saved-colors

--- routes/savedColors.js
+++ routes/savedColors.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const savedColorsController = require('../controllers/savedColors')
+const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', savedColorsController.getColors)
+router.get('/', ensureAuth, savedColorsController.getColors)
 
 router.post('/saveColor', savedColorsController.createColor)
 
