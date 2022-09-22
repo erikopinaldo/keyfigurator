c37498823b54da2bcbc27e49bc1633ffe1b38045
Erik Opinaldo <erikopinaldo@gmail.com>
2022-09-21 20:24:54 -0400

    working fetch request redirect for form submission

--- public/script.js
+++ public/script.js
@@ -1,3 +1,87 @@
+// Form init
+
+let form;
+
+function findElements() {
+    form = document.querySelector('form');
+}
+
+function showMessage(data) {
+    alert(data.message);
+}
+
+function onSuccess(data) {
+    showMessage(data);
+}
+
+function onError(data) {
+    console.error(data);
+}
+
+function collectData(currentForm) {
+    // console.log(currentForm)
+    // console.log(currentForm.childNodes[5].childNodes[3].value)
+    // console.log(currentForm.childNodes[1].childNodes[3].value)
+    
+    let formBackgroundColor = currentForm.childNodes[1].childNodes[3].value
+    let formCaseColor = currentForm.childNodes[3].childNodes[3].value
+    let formKeysColor = currentForm.childNodes[5].childNodes[3].value
+
+    let colorObject = {
+        backgroundColor: formBackgroundColor,
+        caseColor: formCaseColor,
+        keysColor: formKeysColor
+    }
+
+    // let formData = new FormData(currentForm)
+    // console.log(formData)
+    // return new FormData(currentForm);
+    return colorObject
+}
+
+function setOptions(currentForm) {
+    console.log(currentForm)
+    let formBackgroundColor = currentForm.childNodes[1].childNodes[3].value
+    let formCaseColor = currentForm.childNodes[3].childNodes[3].value
+    let formKeysColor = currentForm.childNodes[5].childNodes[3].value
+    let formName = currentForm.childNodes[7].childNodes[3].value
+
+    return {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({
+            'backgroundColor': formBackgroundColor,
+            'caseColor': formCaseColor,
+            'keysColor': formKeysColor,
+            'colorName': formName
+        })
+    };
+}
+
+function sendForm(currentForm) {
+    return fetch(currentForm.action, setOptions(currentForm));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    const { currentTarget } = event;
+    sendForm(currentTarget)
+        .then(response => response.json())
+        .then(data => onSuccess(data, currentTarget))
+        .catch(onError);
+}
+
+function subscribe() {
+    form.addEventListener('submit', onSubmit);
+}
+
+function init() {
+    findElements();
+    subscribe();
+}
+
+init();
+
 const savedColor = document.querySelectorAll('.saved-color')
 const deleteBtn = document.querySelectorAll('.deleteBtn')
 
